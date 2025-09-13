1b20c2b9b8fef630804e3020b1ea05282423edc0
samTime101 <srijanregmi44@gmail.com>
2025-09-13 15:27:27 +0545

    feat: event controller

---
+++ src/controllers/events/create.controller.ts
@@ -0,0 +1,69 @@
+// SAMIP REGMI
+// SEP 13 2025
+// create.controller.ts
+
+import type { Request, Response } from "express";
+// ERROR/SUCCESS RESPONSE IMPORT
+import { ErrorResponse, SuccessResponse } from '@/dtos/index.js';
+
+// SERVICE IMPORT
+import createEventService from "@/services/event/create.service";
+// STATUS CODE IMPORTS
+import { HTTP } from '@/utils/constants';
+// INTERFACE FOR REQUEST 
+import type { Event } from '@/utils/types/event';
+
+
+class CreateEventController {
+    // CONTROLLER METHOD FOR CREATING EVENT
+    async createEvent(req: Request, res: Response) {
+        try {
+            // POST REQUEST IS SENT VIA BODY WITH FILEDS FROM **Event** INTERFACE
+            const { title, description, status, EventSchedule }: Event = req.body;
+
+            // IF NOT ALL FIELDS ARE GIVEN
+            if (!title || !description || !status || !EventSchedule) {
+                const errorResponse = new ErrorResponse({
+                    error: 'ALL FIELDS ARE REQUIRED',
+                    // I HAVE NOT KEPT **detail** FIELD , DEFAULT MA NULL JANXA HAI 
+                    code: HTTP.BAD_REQUEST
+                });
+                return res.status(HTTP.BAD_REQUEST).json(errorResponse);
+            }
+            // CALLING SERVICE TO CREATE EVENT
+            const [error, result] = await createEventService({
+                title,
+                description,
+                status,
+                EventSchedule
+            });
+            // IF ERROR OCCURS WHEN CREATING EVENT
+            if (error) {
+                const errorResponse = new ErrorResponse({
+                    error,
+                    code: HTTP.BAD_REQUEST
+                });
+                return res.status(HTTP.BAD_REQUEST).json(errorResponse);
+            }
+            
+            // EVENT CREATE HUDA 
+            const successResponse = new SuccessResponse({
+                message: 'EVENT CREATION SUCCESS',
+                data: result,
+                code: HTTP.CREATED
+            });
+            return res.status(HTTP.CREATED).json(successResponse);
+            // IF ANY UNEXPECTED ERROR OCCURS
+        } catch (err) {
+            const errorResponse = new ErrorResponse({
+                error: 'INTERNAL SERVICE ERROR',
+                code: HTTP.INTERNAL
+            });
+            return res.status(HTTP.INTERNAL).json(errorResponse);
+        }
+    }
+}
+
+// I HAVE CREATED AND EXPORTED, YOU CAN DIRECTLY EXPORT INSTANCE LIKE **AYUSH** IN `admin.auth.controller.ts`
+const createEventController = new CreateEventController();
+export default createEventController;
