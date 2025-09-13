01dbee578277ff3670c7ec0bd6a89d9b20513b8a
samTime101 <srijanregmi44@gmail.com>
2025-09-13 16:03:54 +0545

    fix: stricter type declaration

--- src/utils/types/event.ts
+++ src/utils/types/event.ts
@@ -1,14 +1,23 @@
 // SAMIP REGMI
 // SEP 13 2025
-export interface Event {
-    title: string;
-    description: string;
-    status: string;
-    EventSchedule: EventSchedule[];
-}
-export interface EventSchedule {
-    startDate: string;
-    endDate: string;
-    description: string;
-    event: Event;
-}
+import { z } from 'zod';
+
+
+// *.strict()* PREVENTS ADDITIONAL FIELDS OTHER THAN DEFINED ONES
+export const EventSchedule  = z.object({
+    startDate: z.string(),
+    endDate: z.string(),
+    description: z.string(),
+    
+}).strict();
+
+export const Event = z.object({ 
+    title: z.string(),
+    description: z.string(),
+    status: z.string(),
+    EventSchedule: z.array(EventSchedule),
+}).strict();
+
+// TYPE EXPORTS
+export type Event = z.infer<typeof Event>;
+export type EventSchedule = z.infer<typeof EventSchedule>;
