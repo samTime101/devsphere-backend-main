3ee04aa066d68faae4bf2469a28a485b3f8f82e2
samTime101 <srijanregmi44@gmail.com>
2025-09-13 22:01:44 +0545

    feat: db implementation

--- src/services/event/create.service.ts
+++ src/services/event/create.service.ts
@@ -4,8 +4,8 @@
 
 // **TODO**-> IMPLEMENT DB INTEGRATION
 
-// import { prismaSafe } from '@/lib/prismaSafe.ts';
-// import prisma from '@/db/prisma.js';
+import { prismaSafe } from '@/lib/prismaSafe.ts';
+import prisma from '@/db/prisma.js';
 
 
 // INTERFACE IMPORT
@@ -18,8 +18,22 @@ import type { Event } from '@/utils/types/event';
 
 async function createEventService(eventData: Event): Promise<[string | null, Event | null]> {
     try {
+        // SEND TO DB
+        const [dbError, dbEvent] = await prismaSafe(
+            prisma.events.create({
+                data: {
+                    name: eventData.name,
+                    description: eventData.description,
+                    status: eventData.status,
+                    EventSchedule: { create: eventData.eventSchedule },
+                },
+            })
+        );
+        // IF DB ERROR OCCURS
+        if (dbError) {
+            return ['DATABASE ERROR OCCURRED', null];
+        }
         const createdEvent: Event = {
-            // ALL EVENT DATA (title, description, status, EventSchedule)
             ...eventData,
         };
         // ONLY SEND EVENT CREATION DATA !DONT SEND ERROR MESSAGE SEND NULL INSTEAD
