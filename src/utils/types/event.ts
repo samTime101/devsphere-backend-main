f8b8a3038a35bce55086c1851e98f13eed214a2e
samTime101 <srijanregmi44@gmail.com>
2025-09-13 16:23:26 +0545

    fix: renamed to schemas and type export

--- src/utils/types/event.ts
+++ src/utils/types/event.ts
@@ -4,20 +4,20 @@ import { z } from 'zod';
 
 
 // *.strict()* PREVENTS ADDITIONAL FIELDS OTHER THAN DEFINED ONES
-export const EventSchedule  = z.object({
+export const eventScheduleSchema  = z.object({
     startDate: z.string(),
     endDate: z.string(),
     description: z.string(),
     
 }).strict();
 
-export const Event = z.object({ 
+export const eventSchema = z.object({ 
     title: z.string(),
     description: z.string(),
     status: z.string(),
-    EventSchedule: z.array(EventSchedule),
+    EventSchedule: z.array(eventScheduleSchema),
 }).strict();
 
 // TYPE EXPORTS
-export type Event = z.infer<typeof Event>;
-export type EventSchedule = z.infer<typeof EventSchedule>;
+export type Event = z.infer<typeof eventSchema>;
+export type EventSchedule = z.infer<typeof eventScheduleSchema>;
