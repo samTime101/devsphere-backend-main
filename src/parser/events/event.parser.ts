24d5c9f648612289e64148054e9c2b41b7662f81
samTime101 <srijanregmi44@gmail.com>
2025-09-13 16:24:20 +0545

    fix: zodschema import  and type import

--- src/parser/events/event.parser.ts
+++ src/parser/events/event.parser.ts
@@ -1,22 +1,22 @@
 // SAMIP REGMI
 // SEP 13 2025
 // EVENT TYPE IMPORT
-import type { Event as EventType } from '@/utils/types/event';
+import type { Event } from '@/utils/types/event';
 
 // ZOD SCHEMA IMPORT FOR PARSING AND VALIDATION
-import { Event } from '@/utils/types/event';
+import { eventSchema } from '@/utils/types/event';
 
 
 // IF SUCCESS SEND [null, parsedData] 
 // IF FAILURE SEND [errorMessage, null]
-async function eventParser(data: Event): Promise<[string | null, EventType | null]> {
+async function eventParser(data: Event): Promise<[string | null, Event | null]> {
     // PARSING 
-    const parsedEvent = Event.safeParse(data);
+    const parsedEvent = eventSchema.safeParse(data);
     // IF PARSING FAILS
     if (!parsedEvent.success) {
         return ['INVALID DATA FORMAT', null];
     }
-    const eventData: EventType = parsedEvent.data;
+    const eventData: Event = parsedEvent.data;
     return [null, eventData];
 }
 
