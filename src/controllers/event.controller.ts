d76d7a357c942cd1dba49b37b82eb08ff7cec820
samTime101 <srijanregmi44@gmail.com>
2025-09-14 11:47:37 +0545

    fix: stricter type

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -3,11 +3,12 @@
 // event.controller.ts
 
 import createEventController from "@/controllers/events/create.controller";
+import type { Request, Response } from "express";
 
 // REMOVEEVENT AND EDIT EVENT LEFT
 
 class EventController {
-    async createEvent(req: any, res: any) {
+    async createEvent(req: Request, res: Response) {
         return createEventController.createEvent(req, res);
     }
 }
