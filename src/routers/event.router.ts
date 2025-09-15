affec9066482d9fbf6909f84285534324e7127dd
samTime101 <srijanregmi44@gmail.com>
2025-09-15 13:21:31 +0545

    feat: all in one file

--- src/routers/event.router.ts
+++ src/routers/event.router.ts
@@ -2,16 +2,22 @@
 // SEP 14 2025
 // create.router.ts
 
+// MODIFIED ON SEP 15
+// ADDED GET EVENT CONTROLLER( SINGLE ID WALA ) SEP 15 2025
+
 // ROUTER IMPORT
 import { Router } from 'express';
 // CONTROLLER IMPORT
-import eventController from '@/controllers/event.controller';
+import { eventController } from '@/controllers/event.controller';
+
 
 // ROUTER INITIALIZATION
 const eventRouter = Router();
 
 // ROUTE DEFINITION
 eventRouter.post('/create', eventController.createEvent);
+eventRouter.get('/id/:id', eventController.getEvent);
+eventRouter.get('/list', eventController.listEvent);
 
 // EXPORTING THE ROUTER
-export default eventRouter;
+export default eventRouter; 
