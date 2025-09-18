3dd4507bba84c8a6463f1eb48d734d073a2d6aef
samTime101 <srijanregmi44@gmail.com>
2025-09-18 10:55:56 +0545

    add: event delete route

--- src/routers/event.router.ts
+++ src/routers/event.router.ts
@@ -5,11 +5,14 @@
 // MODIFIED ON SEP 15
 // ADDED GET EVENT CONTROLLER( SINGLE ID WALA ) SEP 15 2025
 
+
+// REFERENCE FILE: https://github.com/samTime101/devsphere-backend-main/blob/develop/CONTRIBUTING.md#route-file-structure
+
 // ROUTER IMPORT
 import { Router } from 'express';
 // CONTROLLER IMPORT
 import { eventController } from '@/controllers/event.controller';
-import { authMiddleware } from '@/middleware/auth.middleware';
+import { authMiddleware, isAdmin } from '@/middleware/auth.middleware';
 
 // ROUTER INITIALIZATION
 const eventRouter = Router();
@@ -22,6 +25,10 @@ eventRouter.get('/:id',eventController.getEvent);
 eventRouter.use(authMiddleware);
 eventRouter.post('/', eventController.createEvent);
 eventRouter.patch('/:id',eventController.updateEvent);
+
+// ADMIN ROUTES
+eventRouter.delete('/:id', isAdmin, eventController.deleteEvent);
+
 
 // EXPORTING THE ROUTER
 export default eventRouter; 
