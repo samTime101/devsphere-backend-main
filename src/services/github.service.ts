8d702eead9b74e5be247c08398bfe768a396cc0f
adgator101 <aadityathapa977@gmail.com>
2025-09-29 22:23:39 +0545

    feat(contributor): enhance contributor fetching and existence checking methods

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,21 +1,24 @@
-import prisma from "@/db/prisma";
-import { prismaSafe } from "@/lib/prismaSafe";
-import axios from "axios";
+import prisma from '@/db/prisma';
+import { prismaSafe } from '@/lib/prismaSafe';
+import type { GithubContributor } from '@/types/github.types';
+import axios from 'axios';
 
 class GithubServices {
-  async fetchContributors(repoName: string) {
+  async fetchContributors(
+    repoName: string
+  ): Promise<{ success: boolean; data?: GithubContributor[]; error?: string }> {
     try {
       const token = process.env.GITHUB_TOKEN;
       if (!token) {
-        console.log("No github token found");
-        return { success: false, error: "No GitHub token found" };
+        console.log('No github token found');
+        return { success: false, error: 'No GitHub token found' };
       }
       const repositoryResponse = await axios.get(
         `https://api.github.com/repos/BIC-Devsphere/${repoName}/contributors`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            "X-GitHub-Api-Version": "2022-11-28",
+            'X-GitHub-Api-Version': '2022-11-28',
           },
         }
       );
@@ -37,44 +40,20 @@ class GithubServices {
       }
 
       if (!contributors) {
-        return { success: false, error: "No contributors found" };
+        return { success: false, error: 'No contributors found' };
       }
 
       return { success: true, data: repositoryResponse.data };
     } catch (error) {
-      return { success: false, error: error };
+      return { success: false, error: 'Internal Server error' };
     }
   }
 
   async fetchContributorDetail(githubUsername: string) {
     try {
       const contributorResponse = await axios.get(`https://api.github.com/users/${githubUsername}`);
-      console.log("Contributor detail", contributorResponse.data);
+      console.log('Contributor detail', contributorResponse.data);
       return { success: true, data: contributorResponse.data };
-    } catch (error) {
-      return { success: false, error: error };
-    }
-  }
-
-  async checkContributorExistence(githubUsername: string) {
-    try {
-      const [error, contributorResult] = await prismaSafe(
-        prisma.contributor.findFirst({
-          where: {
-            githubUsername: githubUsername,
-          },
-        })
-      );
-
-      if (error) {
-        return { success: false, error: error };
-      }
-
-      if (!contributorResult) {
-        return { success: false, error: "Could not find contributor" };
-      }
-
-      return { success: true, data: contributorResult };
     } catch (error) {
       return { success: false, error: error };
     }
